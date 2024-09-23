4ec7613fcb3343585c195375489a9bed1867c068
ChristoLappas <christos.lappas@student.pxl.be>
2024-09-23 14:47:22 +0200

    create project + dependencies

---
+++ backend-java/e-commerce-platform/notification-service/src/main/java/be/pxl/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
