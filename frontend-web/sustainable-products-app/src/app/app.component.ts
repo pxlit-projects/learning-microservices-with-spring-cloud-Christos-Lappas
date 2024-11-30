72c96f8472497d90dad8c29f6732750e9f995c9b
ChristoLappas <christos.lappas@student.pxl.be>
2024-11-30 18:22:59 +0100

    login/logout

--- frontend-web/sustainable-products-app/src/app/app.component.ts
+++ frontend-web/sustainable-products-app/src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { AdminComponent } from './admin/admin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CustomerComponent, AdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sustainable-products-app';
+  private readonly storageKey = 'userRole';
+  username: string = '';
+  password: string = '';
+
+  setUserRole(): void {
+    if (this.username === 'admin' || this.username === 'customer') {
+      localStorage.setItem(this.storageKey, this.username);
+    } else {
+      throw new Error('Invalid role. Role must be either "admin" or "customer".');
+    }
+  }
+
+  getUserRole(): 'admin' | 'customer' | null {
+    const role = localStorage.getItem(this.storageKey);
+    return role === 'admin' || role === 'customer' ? role : null;
+  }
+
+
+  isAdmin(): boolean {
+    return this.getUserRole() === 'admin';
+  }
+
+  isCustomer(): boolean {
+    return this.getUserRole() === 'customer';
+  }
+  
 }
---
+++ frontend-web/sustainable-products-app/src/app/customer/customer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer.component.html',
+  styleUrl: './customer.component.css'
+})
+export class CustomerComponent {
+
+  clearUserRole(): void {
+    localStorage.removeItem('userRole');
+  }
+
+}
